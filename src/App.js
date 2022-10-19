b37d1fe29aef9e54b34c8b4aece2a526437c9bdc
Giorgi Chkhikvadze <giorgichxikvadze972@gmail.com>
2022-10-19 15:31:53 +0400

    Made global styles for text input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import Button1 from "./styled-components/Inputs/Buttons/Button1";
-import Button2 from "./styled-components/Inputs/Buttons/Button2";
-import Button3 from "./styled-components/Inputs/Buttons/Button3";
-import ArrowRight from './assets/shared/desktop/icon-arrow-right.svg';
+import TextInput from "./styled-components/Inputs/TextInput";
 
 function App() {
   return (
     <>
-      <Button1>Button1</Button1>
-      <Button2>Button2</Button2>
-      <Button3>Button3 <img src={ArrowRight} alt="Arrow right" /></Button3>
+      <TextInput error='Hu' placeholder="Hu" label="Rassaa" />
     </>
   );
 }
---
+++ src/styled-components/Inputs/TextInput.jsx
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+const TextInput = (props) => {
+  return (
+    <Label isError={props.isError} htmlFor={props.id}>
+      <LabelDiv isError={props.isError}>
+        <span>{props.label}</span>
+        {props.isError && <span>{props.error}</span>}
+      </LabelDiv>
+      <Input id={props.id} type="text" placeholder={props.placeholder} />
+    </Label>
+  );
+}
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: -0.214286px;
+  color: #000000;
+  background: #FFFFFF;
+
+`;
+
+const Input = styled.input`
+  padding: 0 24px;
+  border: 1px solid #CFCFCF;
+  border-radius: 8px;
+  height: 56px;
+  caret-color: #D87D4A;
+
+  
+  &::placeholder {
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 19px;
+    letter-spacing: -0.25px;
+    color: rgba(0, 0, 0, 0.4);
+  }
+  
+  &:focus {
+    border-color: #D87D4A;
+  }
+  
+`;
+
+const LabelDiv = styled.div`
+  display: flex;  
+  justify-content: space-between;
+  align-items: center;
+  gap: 8px;
+
+  span {
+    color: ${(props) => (props.isError ? '#CD2C2C' : '#000000')};
+  }
+
+  span:nth-child(2) {
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 16px;
+    letter-spacing: -0.2px;
+  }
+`;
+
+export default TextInput;
